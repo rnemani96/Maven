903aa8a99ac2b50cc4dfb7c8ae1ff085cc662d79
RAMYA <RAMYA@NEMANI>
2018-12-04 23:53:20 +0530

    maven trial.....

---
+++ src/MavenDemo/MavenDemo.java
@@ -0,0 +1,12 @@
+package MavenDemo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MavenDemo {
+@Test
+public void demo()
+{
+	Reporter.log("hiii", true);
+	}
+}
